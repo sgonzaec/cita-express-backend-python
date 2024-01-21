80c626be3617653d6d02fec32bbee078dea57435
Simon Gonzalez <simongonzalezechavarria@gmail.com>
2024-01-21 04:21:45 +0100

    other: change language

--- app/routes/client_routes.py
+++ app/routes/client_routes.py
@@ -3,12 +3,11 @@ from services.client_service import ClientService
 
 client_routes = Blueprint('client_routes', __name__)
 
-
-with open('app/db_config.env.cfg', 'r') as secreto:
-    host = secreto.readline().strip()
-    user = secreto.readline().strip()
-    password = secreto.readline().strip()
-    database = secreto.readline().strip()
+with open('app/db_config.env.cfg', 'r') as secret:
+    host = secret.readline().strip()
+    user = secret.readline().strip()
+    password = secret.readline().strip()
+    database = secret.readline().strip()
 
 @client_routes.route('/api/clients', methods=['GET'])
 def get_clients():
